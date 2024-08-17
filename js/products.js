39df1d7fa91c1923a27752c644329801290a29cc
CamilaScu <camilascutari21@hotmail.com>
2024-08-16 21:28:41 -0300

    Cambios en CSS

--- js/products.js
+++ js/products.js
@@ -30,11 +30,11 @@ fetch(DATOS)
     .then(response => response.json())
     .then(data => {
         const productos = data.products;
-
+        const formatter = new Intl.NumberFormat('es-ES');
         dato1.innerHTML = data.products[0].name;
-        dato2.innerHTML = data.products[0].currency + " " + data.products[0].cost;
+        dato2.innerHTML = data.products[0].currency + " " + formatter.format(data.products[0].cost);
         dato3.innerHTML = data.products[0].description;
-        dato4.innerHTML = data.products[0].soldCount;
+        dato4.innerHTML = "Vendidos: " + data.products[0].soldCount;
 
         const contenedorImagenUno = document.getElementById("dato5");
         const nuevaImagenUno = document.createElement("img");
@@ -43,9 +43,9 @@ fetch(DATOS)
         contenedorImagenUno.appendChild(nuevaImagenUno);
 
         dato6.innerHTML = data.products[1].name;
-        dato7.innerHTML = data.products[1].currency + " " + data.products[1].cost;
+        dato7.innerHTML = data.products[1].currency + " " + formatter.format(data.products[1].cost);
         dato8.innerHTML = data.products[1].description;
-        dato9.innerHTML = data.products[1].soldCount;
+        dato9.innerHTML = "Vendidos: " + data.products[1].soldCount;
 
         const contenedorImagenDos = document.getElementById("dato10");
         const nuevaImagenDos = document.createElement("img");
@@ -54,9 +54,9 @@ fetch(DATOS)
         contenedorImagenDos.appendChild(nuevaImagenDos);
 
         dato11.innerHTML = data.products[2].name;
-        dato12.innerHTML = data.products[2].currency + " " + data.products[2].cost;
+        dato12.innerHTML = data.products[2].currency + " " + formatter.format(data.products[2].cost);
         dato13.innerHTML = data.products[2].description;
-        dato14.innerHTML = data.products[2].soldCount;
+        dato14.innerHTML = "Vendidos: " + data.products[2].soldCount;
 
         const contenedorImagenTres = document.getElementById("dato15");
         const nuevaImagenTres = document.createElement("img");
@@ -66,9 +66,9 @@ fetch(DATOS)
 
 
         dato16.innerHTML = data.products[3].name;
-        dato17.innerHTML = data.products[3].currency + " " + data.products[3].cost;
+        dato17.innerHTML = data.products[3].currency + " " + formatter.format(data.products[3].cost);
         dato18.innerHTML = data.products[3].description;
-        dato19.innerHTML = data.products[3].soldCount;
+        dato19.innerHTML = "Vendidos: " + data.products[3].soldCount;
 
         const contenedorImagenCuatro = document.getElementById("dato20");
         const nuevaImagenCuatro = document.createElement("img");
@@ -77,9 +77,9 @@ fetch(DATOS)
         contenedorImagenCuatro.appendChild(nuevaImagenCuatro);
 
         dato21.innerHTML = data.products[4].name;
-        dato22.innerHTML = data.products[4].currency + " " + data.products[4].cost;
+        dato22.innerHTML = data.products[4].currency + " " + formatter.format(data.products[4].cost);
         dato23.innerHTML = data.products[4].description;
-        dato24.innerHTML = data.products[4].soldCount;
+        dato24.innerHTML = "Vendidos: " + data.products[4].soldCount;
 
         const contenedorImagenCinco = document.getElementById("dato25");
         const nuevaImagenCinco = document.createElement("img");
@@ -90,3 +90,81 @@ fetch(DATOS)
     .catch(error => {
         console.error('Error:', error);
     });
+
+
+    const btnLupa1 = document.getElementById('boton1');
+    const imagen1 = document.getElementById('dato5');
+
+btnLupa1.addEventListener('click', () => {
+    imagen1.style.transform = 'scale(2)';
+    btnLupa1.style.opacity = 0;
+    btnLupa2.style.opacity = 0;
+    btnLupa3.style.opacity = 0;
+    btnLupa4.style.opacity = 0;
+    btnLupa5.style.opacity = 0;
+    btnCerrar.style.display = 'block';
+    overlay.style.display = 'block';
+});
+
+btnCerrar.addEventListener('click', () => {
+  resetZoom();
+});
+
+overlay.addEventListener('click', () => {
+  resetZoom();
+});
+
+function resetZoom() {
+  imagen1.style.transform = 'scale(1)';
+  btnLupa1.style.opacity = 1;
+  btnCerrar.style.display = 'none';
+  overlay.style.display = 'none';
+}
+    const btnLupa2 = document.getElementById('boton2');
+    const imagen2 = document.getElementById('dato10');
+
+btnLupa2.addEventListener('click', () => {
+    imagen2.style.transform = 'scale(2)';
+    btnLupa1.style.opacity = 0;
+    btnLupa2.style.opacity = 0;
+    btnLupa3.style.opacity = 0;
+    btnLupa4.style.opacity = 0;
+    btnLupa5.style.opacity = 0;
+});
+
+const btnLupa3 = document.getElementById('boton3');
+    const imagen3 = document.getElementById('dato15');
+
+btnLupa3.addEventListener('click', () => {
+    imagen3.style.transform = 'scale(2)';
+    btnLupa1.style.opacity = 0;
+    btnLupa2.style.opacity = 0;
+    btnLupa3.style.opacity = 0;
+    btnLupa4.style.opacity = 0;
+    btnLupa5.style.opacity = 0;
+});
+
+const btnLupa4 = document.getElementById('boton4');
+    const imagen4 = document.getElementById('dato20');
+
+btnLupa4.addEventListener('click', () => {
+    imagen4.style.transform = 'scale(2)';
+    btnLupa1.style.opacity = 0;
+    btnLupa2.style.opacity = 0;
+    btnLupa3.style.opacity = 0;
+    btnLupa4.style.opacity = 0;
+    btnLupa5.style.opacity = 0;
+});
+
+    const btnLupa5 = document.getElementById('boton5');
+    const imagen5 = document.getElementById('dato25');
+
+    btnLupa5.addEventListener('click', () => {
+        imagen5.style.transform = 'scale(2)';
+        btnLupa1.style.opacity = 0;
+        btnLupa2.style.opacity = 0;
+        btnLupa3.style.opacity = 0;
+        btnLupa4.style.opacity = 0;
+        btnLupa5.style.opacity = 0;
+    });
+
