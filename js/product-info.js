4b4a7c3b2dcf6e0442d1ab11b3b99fe04bc67996
TaysaE <taay_echenique@hotmail.com>
2024-10-10 11:50:53 -0300

    cambios en modo dia y noche
    se hicieron cambios de CSS, HTML Y JS

--- js/product-info.js
+++ js/product-info.js
@@ -432,7 +432,7 @@ document.addEventListener('DOMContentLoaded', function() {
   function disableNightMode() {
     // Quitar la clase 'night-mode' del cuerpo y de todos los elementos
     document.body.classList.remove('night-mode');
-    document.querySelectorAll('div, .navbar, .breadcrumb, .modal-content, .rating-container, .footer, button').forEach(el => el.classList.remove('night-mode'));
+    document.querySelectorAll('div,.navbar, .breadcrumb, .modal-content, .rating-container, .footer, button').forEach(el => el.classList.remove('night-mode'));
 
     // Guardar el estado en localStorage
     localStorage.setItem('nightMode', 'disabled');
