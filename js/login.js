58d228f998a3928408683da5f3fcdffdfac8ba85
Walki17 <walkii_@hotmail.com>
2024-10-18 22:21:42 -0300

    original

--- js/login.js
+++ js/login.js
@@ -1,25 +1,31 @@
-const user = document.getElementById("user");
+const emails = document.getElementById("email");
 const password = document.getElementById("password");
 
 document.getElementById("botoning").addEventListener('click', function(event) {
-  if (user.value === "" || password.value === "") {
-    alert("Los campos de usuario y contraseña son obligatorios.");
-    event.preventDefault(); // evita que el formulario se envíe automáticamente cuando se hace click en el botón
+  if (emails.value === "" || password.value === "") {
+    alert("Los campos de email y contraseña son obligatorios.");
+    event.preventDefault(); 
+  } else if (!emails.value.includes("@") || !emails.value.includes(".")) {
+    alert('Ingresa un email válido (ejemplo@ejemplo.com)');
+    event.preventDefault(); 
   } else {
-      alert("Ingresaste correctamente, bienvenid@!");     
-      window.location.href = "index.html";
-    } 
+    alert("Ingresaste correctamente, bienvenid@!");     
+    window.location.href = "index.html";
+  }
 });
 
 const formularioLogin = document.querySelector('#login')
 formularioLogin.addEventListener('click', (e)=> {
   e.preventDefault()
 
-  const usuario = document.querySelector('#user').value
+  const email = document.querySelector('#email').value
   const contrasena = document.querySelector('#password').value
 
   const usuariosTotales = JSON.parse(localStorage.getItem('usuarios')) || []
-  usuariosTotales.push({usuario: usuario, contraseña: contrasena})
+  usuariosTotales.push({email: email, contraseña: contrasena})
   localStorage.setItem('usuarios', JSON.stringify(usuariosTotales))
   
 });
+
+
+
